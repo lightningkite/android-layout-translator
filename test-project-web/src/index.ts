868af0240fc7f9a0e0adb4bd4bfa89f6a72ee08d
Joseph Ivie <joseph@lightningkite.com>
2022-01-19 11:12:41 -0700

    Fixing up tests, removed test project junk

--- test-project-web/src/index.ts
+++
@@ -1,9 +0,0 @@
-import './resources/resources.scss'
-import {TestToggleBinding} from "./resources/layouts/TestToggleBinding";
-
-console.log("Inflating TestToggleBinding")
-const x = TestToggleBinding.inflate()
-console.log("Appending view ", x.root)
-document.body.appendChild(x.root)
-console.log("Done")
-console.log(x)
